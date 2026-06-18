7606aae999cfd0a6c9bc429d748f77679ba072c9
Mohan Abeyrathne <mohansampath200311@gmail.com>
2026-06-18 22:01:59 +0530

    Initial commit: Added local LoginForm UI design

---
+++ clinicManagement/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace clinicManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.login_username = new System.Windows.Forms.TextBox();
+            this.login_password = new System.Windows.Forms.TextBox();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.login_btn);
+            this.panel1.Controls.Add(this.login_password);
+            this.panel1.Controls.Add(this.login_username);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(173, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 388);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(18, 20);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(318, 91);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login Account";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Yi Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // login_username
+            // 
+            this.login_username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.login_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_username.Location = new System.Drawing.Point(132, 154);
+            this.login_username.Name = "login_username";
+            this.login_username.Size = new System.Drawing.Size(186, 34);
+            this.login_username.TabIndex = 3;
+            this.login_username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // login_password
+            // 
+            this.login_password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.login_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_password.Location = new System.Drawing.Point(132, 205);
+            this.login_password.Name = "login_password";
+            this.login_password.Size = new System.Drawing.Size(186, 34);
+            this.login_password.TabIndex = 4;
+            this.login_password.UseSystemPasswordChar = true;
+            this.login_password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // login_btn
+            // 
+            this.login_btn.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.login_btn.Font = new System.Drawing.Font("Microsoft Yi Baiti", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_btn.Location = new System.Drawing.Point(156, 268);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(162, 44);
+            this.login_btn.TabIndex = 5;
+            this.login_btn.Text = "Login";
+            this.login_btn.UseVisualStyleBackColor = false;
+            this.login_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Yi Baiti", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(264, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "* Ayurveda Clinic Management System *";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::clinicManagement.Properties.Resources.Screenshot_2026_06_18_184049;
+            this.ClientSize = new System.Drawing.Size(710, 462);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.TextBox login_password;
+        private System.Windows.Forms.TextBox login_username;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
