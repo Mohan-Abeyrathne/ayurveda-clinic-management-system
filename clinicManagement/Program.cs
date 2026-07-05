3f989038ab2c435a45012e83a4761c25f517c8e9
Teesha Matarage <tmatarage01@gmail.com>
2026-07-05 21:57:08 +0530

    Changed the form handler

--- clinicManagement/Program.cs
+++ clinicManagement/Program.cs
@@ -16,7 +16,7 @@ namespace clinicManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DoctorDashboard()); //Form1
+            Application.Run(new Form1()); //Form1
         }
     }
 }
