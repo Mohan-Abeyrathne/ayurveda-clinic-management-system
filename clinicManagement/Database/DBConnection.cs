41e31183677159960fc3505b6728c703ff0ab7f9
Adithi Arundi <adithiarundi@gmail.com>
2026-07-05 19:32:53 +0530

    added diagnosis files to the clinicmanagement folder

---
+++ clinicManagement/Database/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clinicManagement.Database
+{
+    internal class DBConnection
+    {
+        private string connectionString = "server=localhost;database=clinicmanagement;uid=root;pwd=;";
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
