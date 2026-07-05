1e936477885988711374692045547784412136e8
Teesha Matarage <tmatarage01@gmail.com>
2026-07-06 01:06:11 +0530

    Completed the doctor dashboard UI

--- clinicManagement/DoctorDashboard.Designer.cs
+++ clinicManagement/DoctorDashboard.Designer.cs
@@ -35,20 +35,22 @@
             this.btnPrescription = new System.Windows.Forms.Button();
             this.btnLogOut = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Honeydew;
-            this.label1.Location = new System.Drawing.Point(252, 126);
+            this.label1.Location = new System.Drawing.Point(515, 215);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(809, 48);
+            this.label1.Size = new System.Drawing.Size(560, 73);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to the Doctor Dashboard!";
+            this.label1.Text = "Welcome to the";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnPatientFiles
             // 
@@ -56,13 +58,15 @@
             this.btnPatientFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPatientFiles.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPatientFiles.ForeColor = System.Drawing.Color.Honeydew;
-            this.btnPatientFiles.Location = new System.Drawing.Point(115, 241);
+            this.btnPatientFiles.Location = new System.Drawing.Point(176, 156);
             this.btnPatientFiles.Name = "btnPatientFiles";
-            this.btnPatientFiles.Size = new System.Drawing.Size(243, 52);
+            this.btnPatientFiles.Size = new System.Drawing.Size(294, 71);
             this.btnPatientFiles.TabIndex = 1;
             this.btnPatientFiles.Text = "View Patient Files";
             this.btnPatientFiles.UseVisualStyleBackColor = false;
             this.btnPatientFiles.Click += new System.EventHandler(this.btnPatientFiles_Click);
+            this.btnPatientFiles.MouseEnter += new System.EventHandler(this.btnPatientFiles_MouseEnter);
+            this.btnPatientFiles.MouseLeave += new System.EventHandler(this.btnPatientFiles_MouseLeave);
             // 
             // btnDiagnosis
             // 
@@ -70,13 +74,15 @@
             this.btnDiagnosis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDiagnosis.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDiagnosis.ForeColor = System.Drawing.Color.Honeydew;
-            this.btnDiagnosis.Location = new System.Drawing.Point(383, 241);
+            this.btnDiagnosis.Location = new System.Drawing.Point(176, 247);
             this.btnDiagnosis.Name = "btnDiagnosis";
-            this.btnDiagnosis.Size = new System.Drawing.Size(243, 52);
+            this.btnDiagnosis.Size = new System.Drawing.Size(294, 71);
             this.btnDiagnosis.TabIndex = 2;
             this.btnDiagnosis.Text = "Diagnosis";
             this.btnDiagnosis.UseVisualStyleBackColor = false;
             this.btnDiagnosis.Click += new System.EventHandler(this.btnDiagnosis_Click);
+            this.btnDiagnosis.MouseEnter += new System.EventHandler(this.btnDiagnosis_MouseEnter);
+            this.btnDiagnosis.MouseLeave += new System.EventHandler(this.btnDiagnosis_MouseLeave);
             // 
             // btnPrescription
             // 
@@ -84,12 +90,14 @@
             this.btnPrescription.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPrescription.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPrescription.ForeColor = System.Drawing.Color.Honeydew;
-            this.btnPrescription.Location = new System.Drawing.Point(652, 241);
+            this.btnPrescription.Location = new System.Drawing.Point(176, 338);
             this.btnPrescription.Name = "btnPrescription";
-            this.btnPrescription.Size = new System.Drawing.Size(243, 52);
+            this.btnPrescription.Size = new System.Drawing.Size(294, 71);
             this.btnPrescription.TabIndex = 3;
             this.btnPrescription.Text = "Prescriptions";
             this.btnPrescription.UseVisualStyleBackColor = false;
+            this.btnPrescription.MouseEnter += new System.EventHandler(this.btnPrescription_MouseEnter);
+            this.btnPrescription.MouseLeave += new System.EventHandler(this.btnPrescription_MouseLeave);
             // 
             // btnLogOut
             // 
@@ -103,6 +111,8 @@
             this.btnLogOut.TabIndex = 4;
             this.btnLogOut.Text = "Log Out";
             this.btnLogOut.UseVisualStyleBackColor = false;
+            this.btnLogOut.MouseEnter += new System.EventHandler(this.btnLogOut_MouseEnter);
+            this.btnLogOut.MouseLeave += new System.EventHandler(this.btnLogOut_MouseLeave);
             // 
             // button1
             // 
@@ -110,12 +120,27 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Honeydew;
-            this.button1.Location = new System.Drawing.Point(919, 241);
+            this.button1.Location = new System.Drawing.Point(176, 431);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(243, 52);
+            this.button1.Size = new System.Drawing.Size(294, 71);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Therapy Shedules Management";
+            this.button1.Text = "Therapy Schedules Management";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Verdana", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Honeydew;
+            this.label2.Location = new System.Drawing.Point(529, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(487, 53);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Doctor Dashboard!";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // DoctorDashboard
             // 
@@ -124,6 +149,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1284, 719);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLogOut);
             this.Controls.Add(this.btnPrescription);
@@ -148,5 +174,6 @@
         private System.Windows.Forms.Button btnPrescription;
         private System.Windows.Forms.Button btnLogOut;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- clinicManagement/DoctorDashboard.cs
+++ clinicManagement/DoctorDashboard.cs
@@ -42,5 +42,61 @@ namespace clinicManagement
             // 2. Display the form model on screen instantly 
             diagnosisWindow.Show();
         }
+
+        private void btnPatientFiles_MouseEnter(object sender, EventArgs e)
+        {
+            btnPatientFiles.ForeColor = Color.DarkGreen;
+        }
+
+        private void btnPatientFiles_MouseLeave(object sender, EventArgs e)
+        {
+            btnPatientFiles.ForeColor = Color.Honeydew;
+        }
+
+        private void btnDiagnosis_MouseEnter(object sender, EventArgs e)
+        {
+            btnDiagnosis.ForeColor = Color.DarkGreen;
+        }
+
+        private void btnDiagnosis_MouseLeave(object sender, EventArgs e)
+        {
+            btnDiagnosis.ForeColor = Color.Honeydew;
+        }
+
+        private void btnPrescription_MouseEnter(object sender, EventArgs e)
+        {
+            btnPrescription.ForeColor = Color.DarkGreen;
+        }
+
+        private void btnPrescription_MouseLeave(object sender, EventArgs e)
+        {
+            btnPrescription.ForeColor = Color.Honeydew;
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.DarkGreen;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.Honeydew;
+
+        }
+
+        private void btnLogOut_MouseEnter(object sender, EventArgs e)
+        {
+            btnLogOut.ForeColor = Color.DarkGreen;
+        }
+
+        private void btnLogOut_MouseLeave(object sender, EventArgs e)
+        {
+            btnLogOut.ForeColor = Color.Honeydew;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
