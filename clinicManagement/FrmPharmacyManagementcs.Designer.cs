eeeb0f98675ba97d2811fe59ece03dafffd649d9
[Pawara Mahasen] <pawararathnayaka@gmail.com>
2026-07-07 11:24:57 +0530

    Changed the color of pharmacy management

--- clinicManagement/FrmPharmacyManagementcs.Designer.cs
+++ clinicManagement/FrmPharmacyManagementcs.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.pnlSidebar = new System.Windows.Forms.Panel();
-            this.btnDashboard = new System.Windows.Forms.Button();
-            this.btnInventory = new System.Windows.Forms.Button();
-            this.btnAddNew = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnInventory = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
             this.pnlContentStage = new System.Windows.Forms.Panel();
             this.pnlSidebar.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlSidebar
             // 
-            this.pnlSidebar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlSidebar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.pnlSidebar.Controls.Add(this.btnClose);
             this.pnlSidebar.Controls.Add(this.btnAddNew);
             this.pnlSidebar.Controls.Add(this.btnInventory);
@@ -50,52 +50,61 @@
             this.pnlSidebar.Size = new System.Drawing.Size(339, 741);
             this.pnlSidebar.TabIndex = 0;
             // 
-            // btnDashboard
-            // 
-            this.btnDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDashboard.Location = new System.Drawing.Point(102, 153);
-            this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Size = new System.Drawing.Size(137, 85);
-            this.btnDashboard.TabIndex = 0;
-            this.btnDashboard.Text = "📊 Dashboard";
-            this.btnDashboard.UseVisualStyleBackColor = true;
-            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
-            // 
-            // btnInventory
-            // 
-            this.btnInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInventory.Location = new System.Drawing.Point(102, 259);
-            this.btnInventory.Name = "btnInventory";
-            this.btnInventory.Size = new System.Drawing.Size(137, 85);
-            this.btnInventory.TabIndex = 1;
-            this.btnInventory.Text = "📦 Manage Inventory";
-            this.btnInventory.UseVisualStyleBackColor = true;
-            this.btnInventory.Click += new System.EventHandler(this.btnInventory_Click);
-            // 
-            // btnAddNew
-            // 
-            this.btnAddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddNew.Location = new System.Drawing.Point(102, 367);
-            this.btnAddNew.Name = "btnAddNew";
-            this.btnAddNew.Size = new System.Drawing.Size(137, 85);
-            this.btnAddNew.TabIndex = 2;
-            this.btnAddNew.Text = "➕ Add Medicine";
-            this.btnAddNew.UseVisualStyleBackColor = true;
-            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
-            // 
             // btnClose
             // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(102, 480);
+            this.btnClose.Location = new System.Drawing.Point(102, 468);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(137, 85);
             this.btnClose.TabIndex = 3;
             this.btnClose.Text = "❌ Close";
-            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAddNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNew.Location = new System.Drawing.Point(102, 350);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(137, 85);
+            this.btnAddNew.TabIndex = 2;
+            this.btnAddNew.Text = "➕ Add Medicine";
+            this.btnAddNew.UseVisualStyleBackColor = false;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnInventory
+            // 
+            this.btnInventory.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnInventory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventory.Location = new System.Drawing.Point(102, 245);
+            this.btnInventory.Name = "btnInventory";
+            this.btnInventory.Size = new System.Drawing.Size(137, 85);
+            this.btnInventory.TabIndex = 1;
+            this.btnInventory.Text = "📦 Manage Inventory";
+            this.btnInventory.UseVisualStyleBackColor = false;
+            this.btnInventory.Click += new System.EventHandler(this.btnInventory_Click);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.Location = new System.Drawing.Point(102, 132);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(137, 85);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "📊 Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
             // 
             // pnlContentStage
             // 
+            this.pnlContentStage.BackColor = System.Drawing.SystemColors.ControlLight;
             this.pnlContentStage.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlContentStage.Location = new System.Drawing.Point(339, 0);
             this.pnlContentStage.Name = "pnlContentStage";
